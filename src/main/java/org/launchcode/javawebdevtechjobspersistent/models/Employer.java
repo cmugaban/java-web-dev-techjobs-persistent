2d1b79842512fcb3c41e4014b9ebdf7cd7d1ab95
cmugaban <cmugaban@gmail.com>
2021-07-15 18:03:58 -0500

    Not finished with part 4

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,72 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+//    @NotBlank
+    @NotNull
+    @Size(message = "Location must be between 5 and 100 characters.", min = 5, max = 100)
+    private String location;
+
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    public String getLocation() {
+        return location;
+
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+    public Employer() {
+
+    }
 
 
 }
+
+
+//@Entity
+//public class Employer extends AbstractEntity {
+//
+//    @NotBlank(message = "Location is required")
+//    private String location;
+//
+//    @OneToMany
+//    @JoinColumn
+//    private List<Job> employers = new ArrayList<>();
+//
+//    public Employer(String location) {
+//        this.location = location;
+//    }
+//
+//    public Employer() {}
+//
+//    public String getLocation() { return location; }
+//
+//    public void setLocation(String location) {
+//        this.location = location;
+//    }
+//
+//}
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
@@ -1,51 +1,56 @@
+
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.*;
+import java.util.List;
+
 
 @Entity
-public class Job{
+public class Job extends AbstractEntity{
 
-    @Id
-    @GeneratedValue
-    private int id;
+    @ManyToOne
+    private Employer employer;
 
-    private String name;
-
-    private String employer;
-    private String skills;
+    @ManyToMany
+    private List<Skill> skills;
 
     public Job() {
+
+
     }
 
-    public Job(String anEmployer, String someSkills) {
+    public Job(Employer anEmployer, List<Skill> someSkills) {
+
         super();
         this.employer = anEmployer;
         this.skills = someSkills;
+
     }
 
-    // Getters and setters.
+    public Employer getEmployer() {
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmployer() {
         return employer;
+
     }
 
-    public void setEmployer(String employer) {
+    public void setEmployer(Employer employer) {
+
         this.employer = employer;
+
     }
 
-    public String getSkills() {
+    public Iterable<Skill> getSkills() {
+
         return skills;
+
     }
 
-    public void setSkills(String skills) {
+    public void setSkills(List<Skill> skills) {
+
         this.skills = skills;
+
     }
+
+
+
 }
